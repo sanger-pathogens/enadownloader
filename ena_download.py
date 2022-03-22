756ade441693bce45c57c3ba4142b5eb3b4dc710
Guus van de Steeg <gv4@sanger.ac.uk>
2022-03-22 12:36:19 +0000

    add excel writer to metadata class

--- ena_download.py
+++ ena_download.py
@@ -11,6 +11,7 @@ import logging
 import multiprocessing as mp
 import os
 import shutil
+from collections import defaultdict
 from typing import Iterable
 import urllib.request as urlrequest
 from distutils.util import strtobool
@@ -21,6 +22,8 @@ from urllib.error import URLError
 
 import requests
 import xmltodict
+
+from excel import Data, ExcelWriter, FileHeader
 
 
 class ENAObject:
@@ -265,6 +268,55 @@ class ENAMetadata:
             raise
         return names
 
+    def to_excel(self):
+        """Generates one .xls file per ENA project to be fed into PathInfo legacy pipelines"""
+
+        studies = defaultdict(list)
+        for row in self.metadata:
+            studies[row["study_accession"]].append(row)
+
+        for study in studies.values():
+            fh = FileHeader(
+                "Pathogen Informatics",
+                "PaM",
+                "path-help",
+                study[0]["instrument_platform"],
+                study[0]["study_title"],
+                1,
+                "18/03/2025",
+                study[0]["study_accession"],
+            )
+
+            data = []
+            for row in study:
+                if not row["fastq_ftp"].strip():
+                    logging.warning(f"Can't find ftp for accession: {row['run_accession']}. Skipping.")
+                    continue
+
+                files = row["fastq_ftp"].split(";")
+
+                if len(files) == 1:
+                    filename = basename(files[0])
+                    matefile = None
+                else:
+                    try:
+                        filename = basename([f for f in files if "_1" in f][0])
+                        matefile = basename([f for f in files if "_2" in f][0])
+                    except IndexError:
+                        logging.warning(f"Can't correctly extract filename and matefile paths from row: {row}.")
+                        continue
+
+                data.append(
+                    Data(
+                        filename=filename,
+                        mate_file=matefile,
+                        sample_name=row["sample_accession"],
+                        taxon=int(row["tax_id"]),
+                    )
+                )
+
+                writer = ExcelWriter(fh, data)
+                writer.write(f"{fh.study_accession_number.value}.xls")
 
 class ENADownloader:
     class InvalidRow(ValueError):
@@ -626,4 +678,5 @@ if __name__ == "__main__":
         metadata_obj=enametadata,
         project_id="PROJECT_ID",
     )
-    enadownloader.download_project_fastqs()
+    # enadownloader.download_project_fastqs()
+    enametadata.to_excel()
